d0a44c9626fb8581cff63343ac8ede840964d7f1
Felipe Scrochio Custódio <felipe.crochi@gmail.com>
2017-04-02 14:00:00 +0100

    Started exercise 02

---
+++ src/ex02.c
@@ -0,0 +1,15 @@
+/**
+ * Organização de Arquivos
+ * Exercício Prático 02
+ * Felipe Scrochio Custódio
+ * 9442688
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+
+	return 0;
+}
