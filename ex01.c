2f501cbfaafbc75c17e5e5b5e4584fd5c45e96b6
Felipe Scrochio Custódio <felipe.crochi@gmail.com>
2017-04-07 17:53:27 -0300

    Write to file function done

--- ex01.c
+++ ex01.c
@@ -4,29 +4,32 @@
  * Felipe Scrochio Custódio
  * 9442688
 **/
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-// Registry size
-#define SIZE 40
-// Field size
-#define STRING_SIZE 12
+/**
+ * Registros e Campos de tamanho Fixo
+**/
+#define SIZE 34
+#define FIELD 10
+#define DELIM '|'
 
-// Registry
+// DATA
 typedef struct data {
 
 	char *name;
 	char *street;
 	int number;
-	char *complements;
+	char *complement;
 
 } DATA;
 
+// UTILS
 char* readString() {
 
 	char* string = NULL;
-    char stop = '@';
+    	char stop = '@';
 	int counter = 0;
 
 	while (stop != 10) {
@@ -40,125 +43,172 @@ char* readString() {
 	return string;
 }
 
+// DATA FUNCTIONS
+DATA* createData() {
+	DATA* d = (DATA*)malloc(sizeof(DATA));
+	d->name = NULL;
+	d->street = NULL;
+	d->number = -1;
+	d->complement = NULL;
+	return d;
+}
+
+void printData(DATA* d) {
+	printf("*** REGISTRO ***\n");
+	printf("Nome: %s\n", d->name);
+	printf("Endereço: %s\n", d->street);
+	printf("Número: %d\n", d->number);
+	printf("Complemento: %s\n", d->complement);
+}
+
+DATA* readData(DATA* d) {
+	printf("*** INSERIR REGISTRO ***\n");
+	printf("Nome: ");
+	d->name = readString();
+	printf("Endereço: ");
+	d->street = readString();
+	printf("Número: ");
+	scanf("%d", &d->number);
+	getchar();
+	printf("Complemento: ");
+	d->complement = readString();
+	printf("***\n");
+	return d;
+}
+
+// FILES FUNCTIONS
+
+// WRITE DATA TO FILE
+void writeFile(FILE* fp, DATA **d, int fields) {
+
+	int i, j;
+	for (i = 0; i < fields; i++) {
+
+		// NOME
+		for (j = 0; j < FIELD; j++) {
+			if (j > strlen(d[i]->name) - 1) {
+				fputc('$', fp);
+			} else {
+				fputc(d[i]->name[j], fp);
+			}
+		}
+
+		// RUA
+		for (j = 0; j < FIELD; j++) {
+			if (j > strlen(d[i]->street)) {
+				fputc('$', fp);
+			} else {
+				fputc(d[i]->street[j], fp);
+			}
+		}
+
+		// NÚMERO
+		fwrite(&d[i]->number, sizeof(int), 1, fp);
+
+		// COMPLEMENTO
+		for (j = 0; j < FIELD; j++) {
+			if (j > strlen(d[i]->complement)) {
+				fputc('$', fp);
+			} else {
+				fputc(d[i]->complement[j], fp);
+			}
+		}
+	}
+	rewind(fp);
+}
+
+// LIST ALL DATA FROM FILE
+void readFile(FILE* fp) {
+
+	char* field = NULL;
+	field = (char*)malloc(sizeof(char) * FIELD);
+	fread(field, sizeof(char), FIELD, fp);
+	printf("I just read: ");
+	printf("%s\n", field);
+
+}
+
 int byteOffset(int RRN) {
     return (RRN * SIZE);
 }
 
-DATA writeData();
-void printData(DATA data);
-void get(DATA data, int RRN);
-
-DATA writeData() {
-
-}
-
-DATA readField() {
-    DATA field;
-    printf("Nome:\t");
-    field.name = readString();
-    printf("Rua:\t");
-    field.street = readString();
-    printf("Número:\t");
-    scanf("%d", &field.number);
-    getchar(); // keyboard buffer handling
-    printf("Complemento:\t");
-    field.complements = readString();
-    return field;
-}
-
-void printField(DATA field, int i) {
-    printf("*-----------*\n");
-    printf("Registro %d\n", i);
-    printf("Nome: %s\n", field.name);
-    printf("Rua: %s\n", field.street);
-    printf("Número: %d\n", field.number);
-    printf("Complemento: %s\n", field.complements);
-}
-
-DATA* readData() {
-
-    int menu = -1;
-    int fields = 0;
-    DATA* data = NULL;
-
-    printf("Leitura de Dados\n");
-    while (menu != 2) {
-        printf("Selecione a opção desejada\n");
-        printf("1) Inserir novo registro\n");
-        printf("2) Terminar e escrever arquivo\n");
-        printf(":::\t");
-        scanf("%d", &menu);
-        getchar();
-
-        switch(menu) {
-            case 1:
-                // Ler novo registro
-                data = (DATA*)realloc(data, sizeof(DATA*) * fields + 1);
-                data[fields] = readField();
-                break;
-            case 2:
-                printf("Escrevendo arquivo...\n");
-                break;
-            default:
-                printf("Opção inválida!\n");
-                break;
-        }
-    }
-    return data;
-}
-
-void printFile() {
-
-
-
-
-}
-
-void get(DATA data, int RRN) {
-
-}
-
+// MAIN
 int main(int argc, char *argv[]) {
 
+	FILE* file = NULL;
+	DATA** d = NULL;
+	int fields = 0;
+	char* filename = NULL;
 	int menu = -1;
-	int RRN = 0;
-    FILE *fp = NULL;
-    DATA* data = NULL;
-    char* filename = NULL;
-
-    printf("Insert filename ::: \t");
-    filename = readString();
-
-    fp = fopen("rw", filename);
-    data = readData();
-
-    // escrever dados no arquivo
-
 
 	while (menu != 0) {
-		printf("Select option:\n");
-		printf("1) Print data\n");
-		printf("2) Recover data via RRN\n");
-		printf("0) Exit\n");
-		printf(":::\t");
 
+		printf("Escolha a opção desejada\n");
+		printf("1 - Inserir novo registro\n");
+		printf("0 - Terminar inserção de dados\n");
+		printf("::: ");
 		scanf("%d", &menu);
+		getchar();
+
+		switch (menu) {
+			case 1:
+				// NOVO REGISTRO
+				d = (DATA**)realloc(d, sizeof(DATA*) * fields + 1);
+				d[fields] = createData();
+				d[fields] = readData(d[fields]);
+				fields++;
+			case 0:
+				// TERMINAR
+				printf("Escrever no Arquivo\n");
+				break;
+			default:
+				printf("Opção inválida!\n");
+				break;
+		}
+
+	}
+
+	menu = -1;
+	printf("Filename ::: ");
+	filename = readString();
+	file = fopen(filename, "w");
+	if (!file) printf("ERROR CREATING FILE\n");
+
+	while (menu != 0) {
+
+		printf("Escolha a opção desejada\n");
+		printf("1 - Gravar dados no arquivo\n");
+		printf("2 - Recuperar dados do arquivo\n");
+		printf("3 - Busca por RRN\n");
+		printf("0 - Sair\n");
+		printf("::: ");
+		scanf("%d", &menu);
+
 		switch(menu) {
 			case 1:
-				printFile(fp);
+				// write data
+				printf("Write data to file\n");
+				writeFile(file, d, fields);
 				break;
 			case 2:
-				printf("Insert RRN :::\t");
-				scanf("%d", &RRN);
-				// get(data, RRN);
+				printf("Get data from file\n");
+				// get data
 				break;
-            case 0:
-                printf("Exiting\n");
-                break;
-            default:
-                printf("Invalid option\n");
-                break;
+			case 3:
+				printf("Recover data from RRNn\n");
+				// recover data
+				break;
+			case 0:
+				printf("Au revoir!\n");
+				break;
+			default:
+				printf("Opção inválida!\n");
+				break;
 		}
 	}
+
+	readFile(file);
+	fclose(file);
+
 	return 0;
 }
