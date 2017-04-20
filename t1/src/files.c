7af9a8090e6837d017eace5ddf9baee0a9ef6951
Felipe Scrochio Custódio <felipe.crochi@gmail.com>
2017-04-19 22:52:12 -0300

    Add files functions source

---
+++ t1/src/files.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../includes/csv.h"
+#include "../includes/files.h"
+
+/********************************/
+/*********** DEFINES ************/
+/********************************/
+
+#define DELIM ';'
+
+/********************************/
+/************* UTILS ************/
+/********************************/
+
+/* read string from file */
+char* readfString(FILE* fp, char end) {
+	char* string = NULL;
+	char value = '@';
+	int counter = 0;
+
+	while (value != end) {
+		fscanf(fp, "%c", &value);
+		string = (char*)realloc(string, sizeof(char) * counter + 1);
+		string[counter] = value;
+		counter++;
+	}
+
+	string[counter-1] = '\0';
+	return string;
+}
+
+/********************************/
+/************* FILES ************/
+/********************************/
+
+CSV* readFile(FILE* input) {
+        CSV* csv = newCSV();
+        return csv;
+}
+
+void writeFile(CSV* csv, FILE* output) {
+
+}
