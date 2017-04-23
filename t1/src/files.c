074e2348d11df224ed630bda1122f96455f9c788
Felipe Scrochio Custódio <felipe.crochi@gmail.com>
2017-04-23 00:52:17 -0300

    Modified file writing function

--- t1/src/files.c
+++ t1/src/files.c
@@ -1,3 +1,12 @@
+/********************************
+*
+*       SCC0215 - ORGANIZAÇÃO DE ARQUIVOS
+*       TRABALHO 01
+*
+*
+*
+********************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -74,23 +83,99 @@ CSV* readCSV(FILE* input) {
         return csv;
 }
 
+/********************************/
+/************* WRITING **********/
+/********************************/
+
 void writeString(FILE* output, char* string) {
 	fwrite(string, strlen(string), 1, output);
 }
 
+void writeInt(FILE* output, int number) {
+	fprintf(output, "%d", number);
+
+}
+
 void writeFile(CSV* csv, FILE* output) {
+
 	int i;
 
-	// WRITE NUMBER OF FIELDS
+	char* delimiter = (char*)malloc(sizeof(char) * 1);
+	delimiter[0] = '|';
+
+	char* separator = (char*)malloc(sizeof(char) * 1);
+	separator[0] = '\\';
+
+	// WRITE NUMBER OF REGISTRIES
+	writeInt(output, csv->size);
+	writeString(output, delimiter); // field delimiter
 
 	for (i = 0; i < csv->size; i++) {
+
 		// CNPJ - Fixed
+		writeString(output, csv->companies[i]->CNPJ);
+		writeString(output, delimiter); // field delimiter
+
 	        // socialName - Variable
+	        writeInt(output, strlen(csv->companies[i]->socialName)); // field size
+		writeString(output, csv->companies[i]->socialName);
+		writeString(output, delimiter); // field delimiter
+
 	        // fantasyName - Variable
+		writeInt(output, strlen(csv->companies[i]->fantasyName)); // field size
+		writeString(output, csv->companies[i]->fantasyName);
+		writeString(output, delimiter); // field delimiter
+
 	        // registerDate - Fixed
+		writeString(output, csv->companies[i]->registerDate);
+		writeString(output, delimiter); // field delimiter
+
 	        // cancelDate - Fixed
+		writeString(output, csv->companies[i]->cancelDate);
+		writeString(output, delimiter); // field delimiter
+
 	        // reason - Variable
+	        writeInt(output, strlen(csv->companies[i]->reason)); // field size
+		writeString(output, csv->companies[i]->reason);
+		writeString(output, delimiter); // field delimiter
+
 	        // companyName - Variable
+	        writeInt(output, strlen(csv->companies[i]->companyName)); // field size
+		writeString(output, csv->companies[i]->companyName);
+		writeString(output, delimiter); // field delimiter
+
 	        // CNPJaudit - Fixed
+		writeString(output, csv->companies[i]->CNPJaudit);
+
+		// End of registry
+		writeString(output, separator);
+
 	}
 }
+
+/********************************/
+/************* READING **********/
+/********************************/
+
+void readFixedField(FILE* output);
+void readVariableField(FILE* output);
+
+/********************************/
+/************ SEARCH ************/
+/********************************/
+
+void searchRRN(FILE* output);
+void searchField(FILE* output, int field);
+
+/********************************/
+/*********** REMOVAL ************/
+/********************************/
+
+void removeRRN(FILE* output);
+void removeField(FILE* output, int field);
+
+/********************************/
+/********** COMPACT *************/
+/********************************/
+
+void compacter(FILE* output);
