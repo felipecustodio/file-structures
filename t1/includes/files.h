c2331329442f2eaf99109d4ea01b940718d6e8a9
Felipe Scrochio Custódio <felipe.crochi@gmail.com>
2017-04-23 00:53:14 -0300

    Added headers for files functions

--- t1/includes/files.h
+++ t1/includes/files.h
@@ -1,3 +1,12 @@
+/********************************
+*
+*       SCC0215 - ORGANIZAÇÃO DE ARQUIVOS
+*       TRABALHO 01
+*
+*
+*
+********************************/
+
 #ifndef __FILES_H__
 #define __FILES_H__
 
@@ -9,7 +18,40 @@
 /********************************/
 
 CSV* readCSV(FILE* input);
+
+/********************************/
+/********** WRITING *************/
+/********************************/
+
 void writeString(FILE* output, char* string);
+void writeInt(FILE* output, int number);
 void writeFile(CSV* csv, FILE* output);
+
+/********************************/
+/********** READING *************/
+/********************************/
+
+void readFixedField(FILE* output);
+void readVariableField(FILE* output);
+
+/********************************/
+/************ SEARCH ************/
+/********************************/
+
+void searchRRN(FILE* output);
+void searchField(FILE* output, int field);
+
+/********************************/
+/*********** REMOVAL ************/
+/********************************/
+
+void removeRRN(FILE* output);
+void removeField(FILE* output, int field);
+
+/********************************/
+/********** COMPACT *************/
+/********************************/
+
+void compacter(FILE* output);
 
 #endif
--- t1/src/interface.c
+++ t1/src/interface.c
@@ -12,14 +12,14 @@ void print_in_middle(WINDOW *win, int starty, int startx, int width, char *strin
 {	int length, x, y;
 	float temp;
 
-	if(win == NULL)
+	if (win == NULL)
 		win = stdscr;
 	getyx(win, y, x);
-	if(startx != 0)
+	if (startx != 0)
 		x = startx;
-	if(starty != 0)
+	if (starty != 0)
 		y = starty;
-	if(width == 0)
+	if (width == 0)
 		width = 80;
 
 	length = strlen(string);
